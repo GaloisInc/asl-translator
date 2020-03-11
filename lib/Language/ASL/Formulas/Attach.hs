98526cead31f42e4dfb4f1691330cbcabb820bac
Daniel Matichuk <dmatichuk@galois.com>
2020-03-11 13:51:57 -0700

    don't attempt to read empty spec files
    when deciding whether to attach the archived or in-place formulas,
    assume that an empty in-place formulas file is necessarily invalid and
    used the archived formulas instead

--- lib/Language/ASL/Formulas/Attach.hs
+++ lib/Language/ASL/Formulas/Attach.hs
@@ -19,6 +19,7 @@ import qualified Data.Text.Encoding as T
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Unsafe as UBS
+import qualified System.IO as IO
 import qualified System.IO.Unsafe as IO
 import qualified System.Directory as D
 import qualified Codec.Compression.GZip as GZ
@@ -36,18 +37,29 @@ decodeSrc :: BS.ByteString -> T.Text
 decodeSrc bs = T.decodeUtf8 $ LBS.toStrict $ GZ.decompress $ LBS.fromStrict bs
 
 
+fileCases :: FilePath -> FilePath -> (IO.Handle -> IO a) -> (IO.Handle -> IO a) -> IO a
+fileCases fp fallback f1 f2 = do
+  D.doesFileExist fp >>= \case
+    True -> (IO.withFile fp IO.ReadMode $ \handle -> do
+      sz <- IO.hFileSize handle
+      if sz > 0 then
+        Just <$> f1 handle
+      else return Nothing) >>= \case
+        Just result -> return result
+        Nothing -> IO.withFile fallback IO.ReadMode f2
+    False -> IO.withFile fallback IO.ReadMode f2
+
 -- | If the first file exists, assume it is plaintext - so read it in and gzip it.
 -- If it does not exist, use the backup (assumed to already be gzipped)
 attachFormulasSrc :: FilePath -> FilePath -> TH.ExpQ
 attachFormulasSrc fp fallbackfp = do
-  useprimary <- TH.runIO $ D.doesFileExist fp
-  bs <- case useprimary of
-    True -> do
-      t <- TH.runIO $ T.readFile fp
-      return $ LBS.toStrict $ GZ.compress $ LBS.fromStrict $ T.encodeUtf8 t
-    False -> do
-      TH.runIO $ T.writeFile fp T.empty
-      TH.runIO $ BS.readFile fallbackfp
+  bs <- TH.runIO $ fileCases fp fallbackfp
+    (\handle -> do
+        t <- T.hGetContents handle
+        return $ LBS.toStrict $ GZ.compress $ LBS.fromStrict $ T.encodeUtf8 t)
+    (\handle -> do
+        T.writeFile fp T.empty
+        BS.hGetContents handle)
   TH.qAddDependentFile fallbackfp
   TH.qAddDependentFile fp
   embedByteString bs
