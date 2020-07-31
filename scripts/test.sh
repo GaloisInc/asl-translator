ad4903aa42ee458e9cd95dcd3624e716b645d900
Daniel Matichuk <dmatichuk@galois.com>
2020-07-30 23:55:34 -0700

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,19 +2,12 @@
 
 set -euo pipefail
 
-export PATH=/opt/ghc/bin:/opt/cabal/bin:$PATH
-
-if [[ -f "./cabal.project.newbuild" && ! -f "./cabal.project" ]]; then
-    #cp ./cabal.project.newbuild ./cabal.project
-    echo "foo"
-fi
-
-cabal v2-configure -f asl-lite
-cabal v2-update
-cabal v2-build dismantle-arm-xml
-
+ghc --version
+cabal v2-configure --project-file=cabal.project.newbuild -f asl-lite
+cabal v2-update --project-file=cabal.project.newbuild 
+cabal v2-build --project-file=cabal.project.newbuild dismantle-arm-xml
 
 rm -f ./archived/instructions-norm-lite.what4.gz
 
-cabal v2-test asl-translator-genarm-test
-cabal v2-test asl-translator-formula-test
+cabal v2-test --project-file=cabal.project.newbuild asl-translator-genarm-test
+cabal v2-test --project-file=cabal.project.newbuild asl-translator-formula-test
